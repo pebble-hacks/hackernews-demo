ead265fc558dffe1426f878d8c380a72641179c8
Cherie Williams <cherie@getpebble.com>
2015-09-18 22:44:37 -0400

    Integrate Kimono API

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,5 +1,24 @@
-function sendMessage() {
-    Pebble.sendAppMessage({"title": "Top Hacker News Post"});
+function requestTopPost() {
+    url = "https://www.kimonolabs.com/api/41mi0ghm?apikey=ipemug0Wsffq19Kl4Y1YOd97ERjGMFF2"
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            data = JSON.parse(xhr.responseText);
+            stories = data.results.collection1;
+            stories.forEach(function(e) {
+                if(e.index == 1) {
+                    console.log(e.title.text);
+                    sendMessage(e.title.text);
+                }
+            });
+        }
+    };
+    xhr.open('GET', url);
+    xhr.send();
+}
+
+function sendMessage(message) {
+    Pebble.sendAppMessage({"title": message});
 }
 
 Pebble.addEventListener("ready",
@@ -11,6 +30,6 @@ Pebble.addEventListener("ready",
 Pebble.addEventListener("appmessage",
     function(e) {
         console.log("Received message: " + e.payload.greeting);
-        sendMessage();
+        requestTopPost();
     }
 );
